436eb960e35e090b0e245f4db7f186a58fb11632
Mattytomo365 <matty.tom@icloud.com>
2025-04-05 01:58:26 +0100

    Changes to gitignore

--- ShoppingToYou/obj/Debug/net5.0/ShoppingToYou.AssemblyInfo.cs
+++ ShoppingToYou/obj/Debug/net5.0/ShoppingToYou.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingToYou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4933fc313a508d33fcd129fc4f048a768bc58cd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingToYou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingToYou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
